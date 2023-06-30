b213905cfba2f96732e0232fada798c74328753f
Danh Phan <phandanh2112@gmail.com>
2023-06-30 11:25:47 +0700

    final

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # MQTT_MOSCA-server
+
+# Description:
+    - This system includes 1 Broker (MOSCA server) and 2 Clients (Client using ESP8266 and Client using javascript).
+    - Mosca broker broadcasts messages to clients.
+    - ESP8266 Client subcribes "status of led" topic to control led on board, publishes on "connection of board" topic to notify.
+    - JS Client subcribes "connection of board" topic to receive message about connection of board, publishes on "status of led" topic to control led.
+
+# Software:
+    - Arduino IDE (programming for microcontroller)
+    - MQTT Lens (Chrome Extensions) or MQTT Client (app in CHPlay): check connection to MQTT broker and operation of messages
+    - Visual Studio Code: runs broker and client
+
+# Code:
+    - ESP8266_Client.ino:
+        + Modify Wifi information
+        + Modify MQTT server (your local IP) and port
+    - moscaClient.js
+        + Modify MQTT server (your local IP) and port
--- moscaServer.js
+++ moscaServer.js
@@ -1,44 +1,32 @@
-var mosca = require('mosca');   // Khai báo biến mosca sử dụng các thuộc tính của module mosca
+var mosca = require('mosca');  
 
 var settings = {
-  port: 1883,          // port kết nối đến server
+  port: 1883,          
 };
 
-// Lệnh tạo server sử dụng mosca
 var server = new mosca.Server(settings);
 
 var mongo = require('mongodb')
 var mongc = mongo.MongoClient
 var url = 'mongodb://localhost:27017/mqtt'
 
+
+
 server.on('ready', ()=>{
     console.log('Broker is ready!')
 })
 
-// In ra dòng chữ client connected và id của client khi có sự kiện client kết nối thành công đến server
+// When client connects to broker
 server.on('clientConnected', function(client) {
     console.log(client.id, 'connected');
 });
 
-// In ra dòng chữ client disconnected và id của client khi có sự kiện client ngắt kết nối với server
+// When client disconnects to broker
 server.on('clientDisconnected', function(client) {
     console.log(client.id, 'disconnected');
 });
 
-// In ra message của client gửi ở dạng string khi có sự kiện client publish 1 message
+// Display publish events
 server.on('published', function(packet, client) {
     console.log('Published', packet.payload.toString());
-    // message = packet.payload.toString()
-    // console.log(message)
-    // if(message.slice(0,1) != '{' && message.slice(0,4) != 'mqtt'){
-    //     mongc.connect(url, (error, client)=>{
-    //         var myCol = client.db('mqttJS').collection('mqttJS')
-    //         myCol.insertOne({
-    //             message: message
-    //         }, ()=>{
-    //             console.log('Data is saved to MongoDB')
-    //             client.close()
-    //         })
-    //     })
-    // }
 });
